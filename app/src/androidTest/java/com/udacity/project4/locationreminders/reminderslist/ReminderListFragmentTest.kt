7397d82b1fa9e143d0b2a8f5ff4f62b41a402235
Piotr Liszka <pliszka@corelogic.pl>
2021-03-12 11:05:23 +0100

    Step 10: Test the navigation of the fragments, displayed data on the UI, testing for the error messages.

--- app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
@@ -1,23 +1,30 @@
 package com.udacity.project4.locationreminders.reminderslist
 
-import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.udacity.project4.R
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.util.FakeDataSource
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.junit.Assert.*
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mockito.mock
-import org.mockito.Mockito.verify
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.module
+import org.mockito.Mockito
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
@@ -25,7 +32,66 @@ import org.mockito.Mockito.verify
 @MediumTest
 class ReminderListFragmentTest {
 
-//    TODO: test the navigation of the fragments.
-//    TODO: test the displayed data on the UI.
-//    TODO: add testing for the error messages.
+    private lateinit var fakeDataSource: FakeDataSource
+    private lateinit var reminderListViewModel: RemindersListViewModel
+
+    @Before
+    fun setup() {
+        fakeDataSource = FakeDataSource()
+        reminderListViewModel =
+            RemindersListViewModel(ApplicationProvider.getApplicationContext(), fakeDataSource)
+        stopKoin()
+
+        val myModule = module {
+            single { reminderListViewModel }
+        }
+
+        startKoin {
+            modules(listOf(myModule))
+        }
+    }
+
+    @Test
+    fun displayDataOnUI() = runBlockingTest {
+        fakeDataSource.saveReminder(
+            ReminderDTO(
+                "title",
+                "description",
+                "location",
+                (-360..360).random().toDouble(),
+                (-360..360).random().toDouble()
+            )
+        )
+
+        launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+
+        onView(withText("title")).check(ViewAssertions.matches(withText("title")))
+    }
+
+    @Test
+    fun navigateToAddReminder() = runBlockingTest {
+
+        val scenario = launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+        val navController = Mockito.mock(NavController::class.java)
+        scenario.onFragment {
+            Navigation.setViewNavController(it.view!!, navController)
+        }
+
+        onView(withId(R.id.addReminderFAB)).perform(ViewActions.click())
+
+        Mockito.verify(navController).navigate(
+            ReminderListFragmentDirections.toSaveReminder()
+        )
+    }
+
+    @Test
+    fun errorSnackBackShown() {
+        fakeDataSource.tasks = null
+
+        launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+
+        Thread.sleep(2000)
+        onView(withText("No reminders found"))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
 }
